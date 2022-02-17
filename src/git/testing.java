9ffce4a0a95f1a80e510fd88f576f410a9172fef
habibfedaee <habib.fedaee@gmail.com>
2022-02-16 18:39:50 -0800

    i have updated some code

--- src/git/testing.java
+++ src/git/testing.java
@@ -5,7 +5,7 @@ public class testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git class 03");
-		System.out.println("Water is blue");
+		System.out.println("Water   blue");
 
 	}
 
